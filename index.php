e3c6f83dd7604aa83e69d7ff1899d1c9373e66b9
jlee54 <jonathan_lee7@live.com>
2021-02-14 19:21:53 -0600

    V1

---
+++ index.php
@@ -0,0 +1,131 @@
+<html>
+ <head>
+  <title>CPE Excercise</title>
+  	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+	<link type="text/css" rel="stylesheet" href="//unpkg.com/bootstrap-vue@latest/dist/bootstrap-vue.min.css" />
+
+ 	<script src="https://cdn.jsdelivr.net/npm/vue@2.6.12"></script>
+ 	<script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.21.1/axios.min.js" integrity="sha512-bZS47S7sPOxkjU/4Bt0zrhEtWx0y0CRkhEp8IckzK+ltifIIE9EMIMTuT/mEzoIMewUINruDBIR/jJnbguonqQ==" crossorigin="anonymous"></script>
+ 	<script src="//unpkg.com/bootstrap-vue@latest/dist/bootstrap-vue.min.js"></script>
+
+	<script src="//unpkg.com/bootstrap-vue@latest/dist/bootstrap-vue-icons.min.js"></script>
+ </head>
+ <body>
+   <div id="app" class="container-fluid">
+   		<span> {{ error }}</span>
+   		<div class="parties" >
+	    	<b-card
+	    		v-for="(party, name) in parties"
+			    :title=name
+			    :img-src=party.img_src
+			    :img-alt=name
+			    img-top
+			    tag="article"
+			    class="party mb-2"
+			  >
+			    <b-card-text>
+			    	<h5>Candidate Spending:</h5>
+			    	<div>Average: ${{ formatNumber(party.mean_spending) }} </div>
+			    	<div>Medium: ${{ formatNumber(party.medium_spending) }} </div>
+			    	<div>Total: ${{ formatNumber(party.total_spending) }} </div>
+			    </b-card-text>
+		  	</b-card>
+		</div>
+   		<div class="table-wrapper table-responsive">
+       		<b-table striped hover 
+       				:fields=table.fields
+       				:items=table.data
+       				sticky-header="60vh"
+       				responsive="sm"
+       		></b-table>
+       	</div>
+   </div>
+
+   <script>
+   	let app = new Vue({
+	  el: '#app',
+	  data: {
+	  	error: null,
+	    table: {
+	    	data: [],
+	    	fields: [],
+	    },
+	    parties: [],
+	  },
+	  created() {
+	  	axios.get("http://localhost/CPE/readCSV.php")
+			.then(response => {
+				if (!response.error) {
+					this.parties = response.data.parties;
+					this.table.data = response.data.data;
+
+					// Construct table fields
+					for (let i = 0; i < response.data.fields.length; i++) {
+						let name = response.data.fields[i];
+
+						let field = {
+							"key": name,
+							"sortable": true
+						}
+						this.table.fields.push(field);
+					}
+				} else {
+					this.error = response.data.error;
+				}
+			})
+	  }
+	})
+
+	function formatNumber(x) {
+	    return x.toLocaleString('en-CA', {maximumFractionDigits:2});
+	}
+
+   </script>
+
+   <style>
+   		#app {
+   			margin-top: 50px;
+   		}
+
+   		.parties {
+   		}
+
+   		.parties .party {
+   			margin: 10px;
+   			width: 300px;
+   		}
+
+   		.parties .party img {
+   			height: 320px;
+   		}
+
+   		.table-wrapper table {
+   			max-width: 1350px;
+	  		max-height: 50vh;
+		    display: block;
+		    overflow-y: scroll;
+   		}
+
+   		.table-wrapper th{
+   			white-space: nowrap;
+   		}
+
+   		@media only screen and (min-width: 1024px) {
+   			.parties {
+	   			display: flex;
+	   			justify-content: center;
+	   			margin: 50px;
+	   		}
+
+	   		.table-wrapper {
+	   			display: flex;
+	   			justify-content: center;
+	   		}
+   		}
+   </style>
+ </body>
+ <footer>
+ </footer>
+</html>
---
+++ readCSV.php
@@ -0,0 +1,80 @@
+<?php
+error_reporting(E_ERROR | E_PARSE);
+
+$file_name = "Canadian Candidates Social Media Ad Spend.csv";
+$response = [];
+
+try {
+    $row = 2;
+    if (($handle = fopen($file_name, "r")) !== FALSE) {
+
+        // Get header info
+        $row = fgetcsv($handle);
+        $fields = $row;
+        $party_index = array_search("Party", $row);
+        $facebook_index = array_search("Facebook Spend", $row);
+        $twitter_index = array_search("Twitter Spend", $row);
+        $parties = [];
+
+        // Get row/candidate info
+        while (($row = fgetcsv($handle)) !== FALSE) {
+            $party = $row[$party_index];
+            $parties[$party]["facebook_spending"] += $row[$facebook_index];
+            $parties[$party]["twitter_spending"] += $row[$twitter_index];
+            $total_spending  = $parties[$party]["facebook_spending"] + $parties[$party]["twitter_spending"];
+            $parties[$party]["total_spending"] += $total_spending;
+            $parties[$party]["contributing_spending"][] = $total_spending;
+
+            $response["data"][] = array_combine($fields, $row);
+        }
+
+        // Do calculations per party
+        foreach ($parties as $name => &$party) {
+            $candidate_count = count($party["contributing_spending"]);
+            $total_spending = $party["total_spending"];
+            $party["mean_spending"] = $total_spending / $candidate_count;
+
+            asort($party["contributing_spending"]);
+
+            $halved = ($candidate_count/2) - 1;
+            if (floor($halved) == $halved) {
+                $party["medium_spending"] = ($party["contributing_spending"][$halved] + $party["contributing_spending"][$halved+1])/2;
+            } else {
+                $party["medium_spending"] = $party["contributing_spending"][ceil($halved)];
+            }
+
+            $img_src = null;
+            switch($name) {
+                case "Liberal":
+                    $img_src = "https://pbs.twimg.com/profile_images/1280206591288016896/sIitdJsA_400x400.jpg";
+                    break;
+                case "Conservative":
+                    $img_src = "https://images.glaciermedia.ca/polopoly_fs/1.24121235.1587424083!/fileImage/httpImage/image.jpg_gen/derivatives/landscape_804/1.jpg";
+                    break;
+                case "Green":
+                    $img_src = "https://pbs.twimg.com/profile_images/1298647859580088321/2akKnE4Q_400x400.jpg";
+                    break;
+                case "NDP":
+                    $img_src = "https://pbs.twimg.com/profile_images/1196465881087455232/hFKPjmeb_400x400.png";
+                    break;
+                case "Bloc Quebecois":
+                    $img_src = "https://pbs.twimg.com/profile_images/1339989389112426499/PY4DfAUG_400x400.jpg";
+                    break;
+                default:
+                    break;
+            }
+            $party["img_src"] = $img_src;
+        }
+
+        fclose($handle);
+    } else {
+        $response["error"] = "Count not open csv file.";
+    }
+
+    $response["fields"] = $fields;
+    $response["parties"] = $parties;
+} catch (Exception $e) {
+    // log error
+    $response["error"] = "Unexpected error. Please try again later and contact support if error continues.";
+}
+echo json_encode($response);
